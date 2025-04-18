682b21cf176ee99d51cefd44edcfac86592094ac
Alioliv <alyceoliveira0717@gmail.com>
2025-04-17 21:07:45 -0300

    Exercios

---
+++ js(rafael)/execios 01/script.js
@@ -0,0 +1,23 @@
+
+function somar(){
+    let n1, n2, soma
+
+    n1 = Number(prompt("primeiro numero: "))
+    // n2 = prompt("segundo numero: ")
+    // n2 = Number(n2)
+
+    n2 = Number(promt("sgundo numero: "))
+
+    soma = n1 + n2
+
+    alert ("soma: " + soma)
+}
+
+function diminuir(){
+    let n1, n2
+    n1 = Number(prompt("primeiro numero: "))
+    n2 = Number(prompt("segundo numero: "))
+    diferenca = n1 - n2
+
+    alert ("diferença: " + diferenca)
+}
