14319603bfcef83b927c047c40a79593b4a5e225
Alioliv <alyceoliveira0717@gmail.com>
2025-04-21 17:59:13 -0300

    1faseSENAI

--- js(alex)/lista1/ex.12/ex12.js
+++ js(alex)/lista1/ex.12/ex12.js
@@ -8,6 +8,13 @@
 
 
 let ingrediente1 = (prompt("Digite o primerio ingrediente:"))
-let ingrediente2 = (prompt("Digite o segun ediente:"))
+
+let ingrediente2 = (prompt("Digite o segundo ingrediente:"))
+
+if(ingrediente1 == "pele de sabo" && ingrediente2 == "asa de morcego"){
+    console.log("poção pronta")
+}else{
+    console.log("Miatura errada!")
+}
 
 
