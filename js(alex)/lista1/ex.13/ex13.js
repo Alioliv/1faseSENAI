45d19735977d3d38050d6431a8b56d51801639e3
Aluno SENAI <Aluno@SN-404320.edu.fiesc.com.br>
2025-04-22 20:27:28 -0300

    1faseSENAI

---
+++ js(alex)/lista1/ex.13/ex13.js
@@ -0,0 +1,11 @@
+//Só entra na festa VIP quem estiver na lista de convidados **e** trouxer convite.
+//**Desafio:**
+//Use `if/else` para dizer se pode entrar ou não.
+
+let nome = "Joana" (prompt("Digite seu nome:"))
+let temConvite = "true" (prompt("DIgite o numro do convite:"))
+ if(nome = "joana" || temConvite =="true" ){
+    console.log("estrada permitida!")
+ }else{
+    console.log("sem acesso")
+ }
