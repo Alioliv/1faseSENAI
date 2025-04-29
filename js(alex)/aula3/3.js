177ca54f26bb78159dc421205e2a97624f8acf6c
Alioliv <alyceoliveira0717@gmail.com>
2025-04-29 20:20:40 -0300

    1faseSENAI

---
+++ js(alex)/aula3/3.js
@@ -0,0 +1,16 @@
+//let contador = 1 
+
+//while(contador >= 5){
+   // alert("contador igua a " + contador)
+   // contador++
+    //contado --
+//}
+
+//alet("contador é: " + contador)
+// alert("uma mensagem")
+
+let numero = 0 
+  while (numero >= 20){
+    alert (" soma é igual: " + numero)
+    numero = numero * 2
+}
---
+++ js(alex)/aula3/senha/senha.js
@@ -0,0 +1,17 @@
+function gerarCaracterAleatorio(){
+    const caracteres = "alalalaalalla"
+    const numeroAleatorio = math.flor(Math.random() * caracteres.lergt)
+
+    return caracteres.charAt(numeroAleatorio)
+}
+
+
+function Mostrar(){
+    let quantidae = Number(prompt("Informe quantos caracteres sua senha deve ter - de1 a 1:"))
+    let senha = ""
+    while(quantidae > 0){
+        senha = senha + gerarCaracterAleatorio()
+        quantidae = quantidade - 1
+    }
+    alert("A senhaé: " + senha)
+}
