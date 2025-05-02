126797dc03cb5114455043301321b3aca6d71262
Alioliv <alyceoliveira0717@gmail.com>
2025-05-01 23:34:42 -0300

    1faseSENAI

---
+++ js(alex)/aula2/lista.html.js/ex07/ex07.js
@@ -0,0 +1,22 @@
+/*Solicite ao usuário que insira um número inteiro.
+Calcule o fatorial desse número e exiba o resultado em um alert . (Lembrese de que o fatorial de um número n é o produto de todos os inteiros de 1
+a n ).
+Exemplo= Fatorial de 5 → 5*4*3*2*1 = 120*/
+
+/*for (let i = 1; i <= numero; i++) {
+    fatorial = fatorial * i; // ou fatorial *= i;
+  }*/
+
+function fatorial1(){
+let num = Number(prompt("Digite um número inteiro: "))
+    
+if(num <= 0 ){
+    alert("Por favor, digite um número inteiro positivo.")
+}else{
+  alert( num <= num, num++) 
+  fatorial = num * i
+}
+alert("O fatorial de " + num + "é" + fatorial)
+
+}
+
--- js(alex)/aula2/lista.html.js/ex08/ex08.js
+++ js(alex)/aula2/lista.html.js/ex08/ex08.js
@@ -4,7 +4,17 @@
 //não por 100, exceto se for divisível por 400) e exiba uma mensagem com o
 //resultado em alert .
 
-function anoBissexto(){
-    let ano =(prompt("Digite um ano:"))
-    
+function anoAleatorio(){
+    let anoAleatorio = Number(prompt("Insira um ano: "))
+
+    resto1 = anoAleatorio % 4
+    resto2 = anoAleatorio % 100
+    resto3 = anoAleatorio % 400
+    if(resto1 === 0 && resto2 != 0){
+        alert("Ano bissexto")
+    }else if(resto3 === 0){
+        alert("Ano bissexto")
+    }else{
+        alert("Não é bissexto")
+    }
 }
