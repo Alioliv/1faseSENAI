51866c826b005ea56a28f498999383892b917ed1
Alioliv <alyceoliveira0717@gmail.com>
2025-05-04 01:02:22 -0300

    1faseSENAI

--- js(alex)/lista.hml.js2/ex01/ex01.js
+++ js(alex)/lista.hml.js2/ex01/ex01.js
@@ -4,5 +4,33 @@
 //Exemplo de Saída: "ol@ m*nd#"
 
 function DecodificadorMensagem(){
-    
+    let frase = prompt("Digite uma frase para codificar:")
+
+
+let novaFrase = ""
+let i = 0
+
+
+while (i < frase.length) {
+    let letra = frase[i];
+
+   
+    if (letra === "a" || letra === "A") {
+        novaFrase = novaFrase + "@";
+    } else if (letra === "e" || letra === "E") {
+        novaFrase = novaFrase + "#";
+    } else if (letra === "i" || letra === "I") {
+        novaFrase = novaFrase + "!";
+    } else if (letra === "o" || letra === "O") {
+        novaFrase = novaFrase + "$";
+    } else if (letra === "u" || letra === "U") {
+        novaFrase = novaFrase + "*";
+    } else {
+        
+        novaFrase = novaFrase + letra;
+    }
+
+    i = i + 1
+}
+alert("Frase codificada: " + novaFrase)
 }
