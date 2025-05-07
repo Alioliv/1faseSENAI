abfcd73d4df9d8db7b40d19dc355000bbc8270e1
Alioliv <alyceoliveira0717@gmail.com>
2025-05-07 00:26:18 -0300

    1faseSENAI

--- js(alex)/aula3/listawhile/ex15/ex15.js
+++ js(alex)/aula3/listawhile/ex15/ex15.js
@@ -1,12 +1,13 @@
 /*2. Calcule a multiplicação de 1 a 5 (fatorial de 5).*/
 
 function contador(){
-    let num = 1
-    let fatorial = 0
-    let resultado
-    while(num <=5 ){
-        resultado = num * fatorial
-        alert ("O fatorial de " + num + " é: " + resultado)
-        resultado ++
-    }
+    let numero = 5  
+    let contador = 1
+    let fatorial = numero
+
+ while(contador < numero){
+    fatorial =  fatorial * (numero - contador)
+    contador++
+}
+alert("O fatorial de " + numero + " é: " + fatorial ) 
 }
---
+++ js(alex)/aula3/listawhile/ex16/ex16.js
@@ -0,0 +1,11 @@
+/*3. Conte quantos números pares existem entre 1 e 50.*/
+function contador(){
+    let num = 1
+    let numpares = 0
+
+    while(num <= 50){
+        if (num % 2 === 0) {
+            numpares += numero 
+          }
+    }
+}
