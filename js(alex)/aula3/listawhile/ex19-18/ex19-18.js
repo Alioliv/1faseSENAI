b8b00d8a7c67e343580dde4c6939b65ab423abf8
Alioliv <alyceoliveira0717@gmail.com>
2025-05-13 14:30:06 -0300

    1faseSENAI

--- js(alex)/aula3/listawhile/ex19-18/ex19-18.js
+++ js(alex)/aula3/listawhile/ex19-18/ex19-18.js
@@ -19,9 +19,63 @@ function ex19(){
 /*7. Simule uma senha com 3 tentativas*/
 
 function ex20(){
-    let senha = 1234 
-    let tentativas = 3
+    let senha = "12345"
+    let senhaDigitada = ""
+    let tentativas = 0
+    let maxTentativas = 3
 
-    while(tentativas <=3 )
+    while (senhaDigitada != senha && tentativas < maxTentativas){
+        senhaDigitada = prompt("Digite a senha: ")
+        tentativas++
+
+        if (senhaDigitada == senha){
+            alert ("Senha correta!")
+        }else{
+            alert ("Senha incorreta!")
+        }
+    }
     
 }
+
+/*8. Conte os números de 1 a 50, mas pare se chegar a 30.*/
+
+function ex21(){
+     let num = 1
+    while (num <= 50){
+        alert ("O número é: " + numero)
+        num++
+        if (num === 30){
+            alert ("O número 30 foi encontrado")
+            break
+        }
+    }
+}
+
+/*9. Conte até 100, mas exiba uma mensagem especial no 50.*/
+
+function ex22(){
+     let num = 0
+    while (num <= 100){
+        alert ("O número é: " + numero)
+        num++
+        if (num === 50){
+            alert ("O número 50 foi encontrado")
+            
+        }
+    }
+}
+
+/*10. Conte de 10 a 0 e diga “Lançamento!” ao final.*/
+
+function ex23(){
+    let num = 0 
+
+    while (num > 10) {
+        alert("O número é: "+ num)
+        num++
+        if (num === 10) {
+            alert("Lançamento")
+        }
+    }
+}
+
