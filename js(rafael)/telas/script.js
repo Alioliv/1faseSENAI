d978c7b13c127c1acd56c2753e25fa7ee6a74eb1
Aluno SENAI <Aluno@SN-404305.edu.fiesc.com.br>
2025-05-29 22:15:13 -0300

    1faseSENAI

---
+++ js(rafael)/telas/script.js
@@ -0,0 +1,80 @@
+/*let nome, email, senha declarar um por vez*/
+// usuario para agrupar todos elementos 
+//document.getElementById("inputLogNome").focus()
+//mostrarLogin()
+
+inicializar()
+
+let usuario = {
+    nome: "um",
+    email: "dois",
+    senha: "nuvem",
+}
+
+function cadastrar(){
+    usuario.nome = document.getElementById("inputCadNome").value
+    usuario.email = document.getElementById("inputCadEmail").value
+    usuario.senha = document.getElementById("inputCadSenha").value
+    alert("Cadastro com sucesso! :D")
+
+    console.log("usuario");
+
+    mostrarLogin()
+    
+}
+
+function logar() {
+    let nome = document.getElementById('inputLogNome').value
+    let senha = document.getElementById('inputLogSenha').value
+    
+    if (nome === usuario.nome && senha === usuario.senha) {
+        alert("login efetuado com sucesso!")
+        mostrarProdutos()
+        limparinput()
+    }else{
+        alert("Login não efetuado, sem sucesso!")
+    }
+}
+
+
+function mostrarLogin(){
+    document.getElementById('login').style.display = 'flex'
+    document.getElementById('cadastro').style.display = 'none'
+    document.getElementById('produtos').style.display = 'none'
+    document.getElementById("inputLogNome").focus()
+
+}
+function mostrarCadastro(){
+    esconderTodos()
+    document.getElementById('cadastro').style.display = 'flex'
+    document.getElementById("inputCadNome").focus()
+   
+}
+
+function mostrarProdutos(){
+    esconderTodos()
+    document.getElementById('cadastro').style.display = 'flex'
+   
+}
+
+function esconderTodos(){
+    document.getElementById('login').style.display = 'none'
+    document.getElementById('cadastro').style.display = 'none'
+    document.getElementById('produtos').style.display = 'none'
+    // esconde pag
+}
+
+function limparinput(){
+    document.getElementById("inputCadNome").value
+    document.getElementById("inputCadEmail").value
+    document.getElementById("inputCadSenha").value
+
+
+    document.getElementById("inputLogNome").value 
+    document.getElementById("inputLogSenha").value
+}
+
+function inicializar(){
+    mostrarLogin()
+    
+}
