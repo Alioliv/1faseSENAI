cc9103c7a4a9b227b94a2014206ff25432a09b78
Alioliv <alyceoliveira0717@gmail.com>
2025-06-09 20:27:38 -0300

    1faseSENAI

--- js(rafael)/telas/script.js
+++ js(rafael)/telas/script.js
@@ -6,9 +6,9 @@
 inicializar()
 
 let usuario = {
-    nome: "um",
-    email: "dois",
-    senha: "nuvem",
+    nome: "",
+    email: "",
+    senha: "",
 }
 
 function cadastrar(){
@@ -19,15 +19,16 @@ function cadastrar(){
 
     console.log("usuario");
 
+    limparImputs()
     mostrarLogin()
     
 }
 
 function logar() {
-    let nome = document.getElementById('inputLogNome').value
-    let senha = document.getElementById('inputLogSenha').value
+    let nome = document.getElementById("inputLogNome").value
+    let senha = document.getElementById("inputLogSenha").value
     
-    if (nome === usuario.nome && senha === usuario.senha) {
+    if ((nome === usuario.nome || nome === usuario.email) && senha === usuario.senha) {
         alert("login efetuado com sucesso!")
         mostrarProdutos()
         limparinput()
@@ -38,29 +39,29 @@ function logar() {
 
 
 function mostrarLogin(){
-    document.getElementById('login').style.display = 'flex'
-    document.getElementById('cadastro').style.display = 'none'
-    document.getElementById('produtos').style.display = 'none'
+    esconderTodos()
+    document.getElementById("login").style.display = 'flex'
     document.getElementById("inputLogNome").focus()
 
 }
 function mostrarCadastro(){
     esconderTodos()
-    document.getElementById('cadastro').style.display = 'flex'
+    document.getElementById("cadastro").style.display = 'flex'
     document.getElementById("inputCadNome").focus()
    
 }
 
 function mostrarProdutos(){
     esconderTodos()
-    document.getElementById('cadastro').style.display = 'flex'
+    document.getElementById("produtos").style.display = 'flex'
+    document.getElementById("navbar").style.display = 'flex' // mostra a nava bar 
    
 }
 
 function esconderTodos(){
-    document.getElementById('login').style.display = 'none'
-    document.getElementById('cadastro').style.display = 'none'
-    document.getElementById('produtos').style.display = 'none'
+    document.getElementById("login").style.display = 'none'
+    document.getElementById("cadastro").style.display = 'none'
+    document.getElementById("produtos").style.display = 'none'
     // esconde pag
 }
 
