165c686b32e3458822c59eb5408409ee11f9fd66
ALICE DE OLIVEIRA COTRIM <alyceoliveira0717@gmail.com>
2025-07-15 21:52:25 -0300

    Alterações

---
+++ js(alex)/Arrays/lista2.js
@@ -0,0 +1,53 @@
+//1. Crie um array com 6 cores e exiba apenas as que começam com a letra "A".
+const cores = ["rosa", "roxo", "azul", "cinza"]
+
+//2. Dado um array de números, multiplique todos os valores por 3 usando .map().
+const num =[ 1,2,3,4,5]
+
+//3. Filtre os nomes de um array que começam com a letra "M".
+const nomes =["ana","julia","clara","maria"]
+
+//4. Usando .reduce(), calcule a multiplicação de todos os números de um array.
+const mul = [3,4,5,6,7]
+
+//5. Verifique qual é o segundo menor número de um array.
+const arry = [2,3,4,5,6]
+
+//6. Crie uma função que retorna apenas os elementos ímpares de um array.
+const arry2 =[1,2,3,4,5,6]
+
+//7. Inverta a ordem das palavras em cada string de um array.
+const palavras = ["um","dois","tres"]
+
+//8. Crie um novo array removendo todas as strings vazias ("") de outro array.
+const arry3 =[" ", "ola", " ", "oi"]
+
+//9. A partir de dois arrays, retorne os elementos que estão somente no primeiro (diferença).
+const arry4 =[1,2,3,4,5]
+const arry6 =[2,3,4]
+
+//10. Em um array de números, encontre a soma dos números maiores que 50.
+const arryNum = [5,6,7,60,70,80]
+
+//11. Crie um array com 10 nomes e retorne apenas os nomes com exatamente 5 letras.
+const arryNomes =["Alice","Clara", "Ana", "Julia"]
+
+//. Agrupe os valores de um array de números por “par” e “ímpar”.
+const num2 =[2,3,4,5,6,7]
+
+//13. Verifique se todos os nomes de um array começam com letra maiúscula.
+
+
+//14. Calcule quantas palavras existem em todas as strings de um array.
+
+//15. Transforme um array de strings em camelCase.
+
+//16. Substitua os números negativos de um array por zero.
+
+//17. Embaralhe as letras de cada string em um array.
+
+//18. Faça uma função que retorne true se dois arrays forem iguais (mesmos valores e ordem).
+
+//19. Crie uma função que retorne quantas palavras terminam com a letra “o” em um array.
+
+//20. Crie um array de 10 objetos com {produto, preco} e filtre os produtos acima de R$50.
